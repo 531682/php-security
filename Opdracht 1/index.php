e885b3d8f549ce8b09215d83bfd95ccc07dca84f
Mitchell Ekelschot <531682@student.glu.nl>
2020-02-19 13:09:12 +0100

    hotfix

--- Opdracht 1/index.php
+++ Opdracht 1/index.php
@@ -7,15 +7,43 @@ if(isset($_POST['submit'])){
      'veld4'=>'filename'
     );
 
-if ($_POST['bool'] == 'bool') { echo "checkbox: TRUE"; } else { echo "checkbox: FALSE";}
-if (isset($_POST['file'])) 
+if (isset($_POST['string']) && $_POST['string'] != null) 
+{ 
+    echo "<br>";
+    echo "string: TRUE"; 
+} else { 
+    echo "<br>";
+    echo "string: FALSE"; 
+} 
+
+if (isset($_POST['int']) && $_POST['int'] != null) 
+{ 
+    echo "<br>"; 
+    echo "int: TRUE"; 
+} else { 
+    echo "<br>";
+    echo "int: FALSE"; 
+} 
+
+if (isset($_POST['bool']) == 'bool') 
+{ 
+    echo "<br>";
+    echo "checkbox: TRUE"; 
+} else { 
+    echo "<br>";
+    echo "checkbox: FALSE";
+} 
+
+if (isset($_POST['file']) && $_POST['file'] != null) 
 { 
     $file = $_POST['file'];
     echo "<br>";
     echo "file: TRUE";
 } else {
+    echo "<br>";
     echo "file: FALSE";
 }
+
  // Controleer hier de vier velden van het formulier op de verschillende tests
  // Veld1
  // Veld2
@@ -29,8 +57,8 @@ if (isset($_POST['file']))
 ?>
 
 <form method="post">
-    <input type="text" name="string" placeholder="String.." required><br>
-    <input type="number" name="int" placeholder="Integer.." required><br>
+    <input type="text" name="string" placeholder="String.."><br>
+    <input type="number" name="int" placeholder="Integer.."><br>
     <input type="checkbox" name="bool" value="bool"><code>bool</code><br>
     <input type="file" name="file"><br>
     <input type="submit" name="submit">
