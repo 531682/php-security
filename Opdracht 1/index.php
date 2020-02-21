3da5aba4f3009f5a918eb95819fe488a120d75b2
Mitchell Ekelschot <531682@student.glu.nl>
2020-02-21 11:15:51 +0100

    update

--- Opdracht 1/index.php
+++ Opdracht 1/index.php
@@ -9,7 +9,14 @@ if(isset($_POST['submit'])){
     
  // Controleer hier de vier velden van het formulier op de verschillende tests
  // Veld1
-if (isset($_POST['string']) && $_POST['string'] != null) 
+if (isset($_POST['string']) && 
+    !empty($_POST['string']) && 
+    $_POST['string'] != null && 
+    !empty($_POST['string']) && 
+    strlen($_POST['string']) != 0 && 
+    !is_null($_POST['string']) && 
+    is_string($_POST['string'])
+    )
 { 
     echo "<br>";
     echo "string: TRUE"; 
@@ -19,7 +26,13 @@ if (isset($_POST['string']) && $_POST['string'] != null)
 } 
 
  // Veld2
-if (isset($_POST['int']) && $_POST['int'] != null) 
+if (isset($_POST['int']) && 
+    $_POST['int'] != null && 
+    !empty($_POST['int']) && 
+    is_numeric($_POST['int']) && 
+    ctype_digit($_POST['int']) && 
+    !is_null($_POST['int'])
+    ) 
 { 
     echo "<br>"; 
     echo "int: TRUE"; 
@@ -29,7 +42,11 @@ if (isset($_POST['int']) && $_POST['int'] != null)
 } 
 
  // Veld3
-if (isset($_POST['bool']) == 'bool') 
+if (isset($_POST['bool']) == 'bool'&& 
+    $_POST['bool'] != null && 
+    !empty($_POST['bool']) && 
+    !is_null($_POST['bool'])
+    ) 
 { 
     echo "<br>";
     echo "checkbox: TRUE"; 
@@ -39,7 +56,11 @@ if (isset($_POST['bool']) == 'bool')
 } 
 
  // Veld4
-if (isset($_POST['file']) && $_POST['file'] != null)
+if (isset($_POST['file']) && 
+    $_POST['file'] != null && 
+    !empty($_POST['file']) && 
+    !is_null($_POST['file'])
+    )
 { 
     $file = $_POST['file'];
     echo "<br>";
@@ -50,7 +71,13 @@ if (isset($_POST['file']) && $_POST['file'] != null)
 }
 
 
+// is_bool($_POST['']);
 
+// is_float($_POST['']);
+// 
+// 
+// is_object($_POST['']);
+// is_array($_POST['']);
 
  /*      NB Veld4 heeft speciale voorwaarden:
  //      - Maximaal 64 characters
